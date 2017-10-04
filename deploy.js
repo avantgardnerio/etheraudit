71f30f8bbe1c000f521d129b620d22d731a47eff
bgardner <bgardner@squarelabs.net>
2017-10-04 16:01:20 -0600

    broken but pushing anyway

--- deploy.js
+++ deploy.js
@@ -9,9 +9,24 @@ const abiDefinition = JSON.parse(compiledCode.contracts[':KingOfTheEtherThrone']
 const VotingContract = web3.eth.contract(abiDefinition)
 const byteCode = compiledCode.contracts[':KingOfTheEtherThrone'].bytecode
 const options = { data: byteCode, from: web3.eth.accounts[0], gas: 4700000 };
-console.log('options=', options);
+//console.log('options=', options);
 VotingContract.new(options, function (err, contract) {
-    console.log('done, ', contract.address);
-    const contractInstance = VotingContract.at(contract.address);
-    console.log('contractInstance, ', contractInstance);
+    if (contract.address)
+        console.log('address=', contract.address);
+    const inst = VotingContract.at(contract.address);
+    //console.log('contractInstance, ', contractInstance);
+    setTimeout(() => {
+        console.log('chainging to', web3.eth.accounts[1])
+        contract.claimThrone.sendTransaction('Brent', {
+            value: 100,
+            from: web3.eth.accounts[1]
+        }, function (err, result) {
+            if (!result) {
+                return;
+            }
+            console.log('done', arguments[1]);
+            const monarch = contract.currentMonarch.call();
+            console.log('monarch=', monarch);
+        })
+    }, 100)
 });
